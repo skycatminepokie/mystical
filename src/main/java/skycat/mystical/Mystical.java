8efffd46a3c38c8e7e80ac1c4b8c0571201ce309
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-10-10 19:45:08 -0700

    Project creation

---
+++ src/main/java/skycat/mystical/Mystical.java
@@ -0,0 +1,10 @@
+package skycat.mystical;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Mystical implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
