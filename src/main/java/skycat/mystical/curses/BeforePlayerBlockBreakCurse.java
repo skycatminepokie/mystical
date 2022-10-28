1ed11ac101246e82526ef8b6a6477c210ed1861f
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-10-27 20:04:55 -0700

    BeforePlayerBlockBreakCurse

---
+++ src/main/java/skycat/mystical/curses/BeforePlayerBlockBreakCurse.java
@@ -0,0 +1,26 @@
+package skycat.mystical.curses;
+
+import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class BeforePlayerBlockBreakCurse extends Curse implements PlayerBlockBreakEvents.Before {
+    private PlayerBlockBreakEvents.Before callback;
+
+    public BeforePlayerBlockBreakCurse(PlayerBlockBreakEvents.Before callback, String name, double difficultyMultiplier) {
+        this.callback = callback;
+    }
+
+    @Override
+    public boolean beforeBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
+        return callback.beforeBlockBreak(world, player, pos, state, blockEntity);
+    }
+
+    @Override
+    void register() {
+         PlayerBlockBreakEvents.BEFORE.register(callback);
+    }
+}
