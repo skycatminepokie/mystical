da23c38fe1e45044acf52e853d6478d84eed6fea
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-10-27 21:02:28 -0700

    CurseHandler started
    I have no clue how the <> works, but it does, so yay!

---
+++ src/main/java/skycat/mystical/curses/CurseHandler.java
@@ -0,0 +1,20 @@
+package skycat.mystical.curses;
+
+import java.util.ArrayList;
+
+public class CurseHandler {
+    public final ArrayList<Curse> curses = new ArrayList<>();
+
+    public CurseHandler() {
+        // Initialize curses
+
+        /*
+        new Curse<>(EntitySleepEvents.START_SLEEPING, ((entity, sleepingPos) -> {
+        }), 0.1);
+        */
+    }
+
+    // Get random curse
+    // Get random curse, but weight chances based on difficulty
+    // Get random curse, but weight chances to get close to a difficulty
+}
