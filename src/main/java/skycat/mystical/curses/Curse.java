0114e449734ff457d9750d4f6583aa78fdde8fc8
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2022-10-27 20:51:02 -0700

    Curses are what I want them to be

--- src/main/java/skycat/mystical/curses/Curse.java
+++ src/main/java/skycat/mystical/curses/Curse.java
@@ -1,6 +1,20 @@
 package skycat.mystical.curses;
 
-public abstract class Curse {
-    abstract void register();
+import net.fabricmc.fabric.api.event.Event;
+
+public class Curse<T> {
+    public final Event<T> event;
+    public final T callback;
+
+    public Curse(Event<T> event, T callback, double difficultyMultiplier) {
+        this.event = event;
+        this.callback = callback;
+        this.difficultyMultiplier = difficultyMultiplier;
+    }
+
     double difficultyMultiplier;
+
+    public void register() {
+        event.register(callback);
+    }
 }
