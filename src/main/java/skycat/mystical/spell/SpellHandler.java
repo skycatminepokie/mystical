41f2b15c10680c49ca6e46951959822f3ff5e49f
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-02-07 13:37:22 -0800

    Allow LevitateConsequence to use other events

--- src/main/java/skycat/mystical/spell/SpellHandler.java
+++ src/main/java/skycat/mystical/spell/SpellHandler.java
@@ -1,11 +1,16 @@
 package skycat.mystical.spell;
 
 import net.fabricmc.fabric.api.entity.event.v1.EntitySleepEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stat;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -22,23 +27,47 @@ import java.util.Scanner;
 
 import static skycat.mystical.Mystical.GSON;
 
-public class SpellHandler implements EntitySleepEvents.StartSleeping, PlayerBlockBreakEvents.After, PlayerBlockBreakEvents.Before {
+public class SpellHandler implements EntitySleepEvents.StartSleeping,
+        EntitySleepEvents.StopSleeping,
+        PlayerBlockBreakEvents.Before,
+        PlayerBlockBreakEvents.After,
+        ServerPlayerEvents.AfterRespawn,
+        ServerEntityCombatEvents.AfterKilledOtherEntity {
     private static final File SAVE_FILE = new File("config/spellHandler.json");
     private final ArrayList<Spell> activeSpells = new ArrayList<>();
 
     public static SpellHandler loadOrNew() {
         try (Scanner scanner = new Scanner(SAVE_FILE)) {
             return GSON.fromJson(scanner.nextLine(), SpellHandler.class);
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             Utils.log(Utils.translateString("text.mystical.spellHandler.loadFailed"), Mystical.CONFIG.failedToLoadSpellHandlerLogLevel());
             return new SpellHandler();
         }
     }
 
+    public void activateNewSpell() {
+        activeSpells.add(SpellGenerator.get());
+    }
+
     @Override
     public void afterBlockBreak(World world, PlayerEntity player, BlockPos pos, BlockState state, BlockEntity blockEntity) {
-        spellsOfHandler(PlayerBlockBreakEvents.After.class).forEach((spell -> ((PlayerBlockBreakEvents.After) spell.getConsequence()).afterBlockBreak(world, player, pos, state, blockEntity)));
+        for (Spell spell : spellsOfHandler(PlayerBlockBreakEvents.After.class)) {
+            ((PlayerBlockBreakEvents.After) spell.getConsequence()).afterBlockBreak(world, player, pos, state, blockEntity);
+        }
+    }
+
+    @Override
+    public void afterKilledOtherEntity(ServerWorld world, Entity entity, LivingEntity killedEntity) {
+        for (Spell spell : spellsOfHandler(ServerEntityCombatEvents.AfterKilledOtherEntity.class)) {
+            ((ServerEntityCombatEvents.AfterKilledOtherEntity) spell.getConsequence()).afterKilledOtherEntity(world, entity, killedEntity);
+        }
+    }
+
+    @Override
+    public void afterRespawn(ServerPlayerEntity oldPlayer, ServerPlayerEntity newPlayer, boolean alive) {
+        for (Spell spell : spellsOfHandler(ServerPlayerEvents.AfterRespawn.class)) {
+            ((ServerPlayerEvents.AfterRespawn) spell.getConsequence()).afterRespawn(oldPlayer, newPlayer, alive);
+        }
     }
 
     @Override
@@ -59,6 +88,24 @@ public class SpellHandler implements EntitySleepEvents.StartSleeping, PlayerBloc
         }
     }
 
+    public <T> void onStatIncreased(PlayerEntity player, Stat<T> stat, int amount) {
+        // Utils.log("stat increased: " + stat.getName() + " amount: " + amount);
+        for (Spell spell : spellsOfStatCure(stat)) {
+            spell.getCure().contribute(player.getUuid(), amount);
+        }
+    }
+
+    @Override
+    public void onStopSleeping(LivingEntity entity, BlockPos sleepingPos) {
+        for (Spell spell : spellsOfHandler(EntitySleepEvents.StopSleeping.class)) {
+            ((EntitySleepEvents.StopSleeping) spell.getConsequence()).onStopSleeping(entity, sleepingPos);
+        }
+    }
+
+    public void removeAllSpells() {
+        activeSpells.clear();
+    }
+
     public void save() {
         try (PrintWriter pw = new PrintWriter(SAVE_FILE)) {
             pw.println(GSON.toJson(this));
@@ -67,15 +114,12 @@ public class SpellHandler implements EntitySleepEvents.StartSleeping, PlayerBloc
         }
     }
 
-    public void activateNewSpell() {
-        activeSpells.add(SpellGenerator.get());
-    }
-
     /**
      * Get all active spells that are using a specified handler
+     *
      * @param clazz The event handler to search for
+     * @param <T>   The type of handlers to return
      * @return An ArrayList of spells that have matching consequences
-     * @param <T> The type of handlers to return
      */
     public <T> ArrayList<Spell> spellsOfHandler(Class<T> clazz) {
         ArrayList<Spell> results = new ArrayList<>();
@@ -97,12 +141,5 @@ public class SpellHandler implements EntitySleepEvents.StartSleeping, PlayerBloc
             }
         }
         return results;
-    }
-
-    public <T> void onStatIncreased(PlayerEntity player, Stat<T> stat, int amount) {
-        // Utils.log("stat increased: " + stat.getName() + " amount: " + amount);
-        for (Spell spell : spellsOfStatCure(stat)) {
-            spell.getCure().contribute(player.getUuid(), amount);
-        }
     }
 }
