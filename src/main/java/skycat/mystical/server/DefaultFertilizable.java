58c245574200e8df8737c3b76f431cba0ad0e9f2
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-02-10 09:37:49 -0800

    Allow all blocks to be fertilized
    but disable it for now

--- src/main/java/skycat/mystical/server/DefaultFertilizable.java
+++ src/main/java/skycat/mystical/server/DefaultFertilizable.java
@@ -7,22 +7,21 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
-import net.minecraft.world.gen.feature.UndergroundConfiguredFeatures;
 
 public interface DefaultFertilizable extends Fertilizable { // This is from MossBlock.java
     @Override
     default boolean isFertilizable(BlockView world, BlockPos pos, BlockState state, boolean isClient) {
-        return world.getBlockState(pos.up()).isAir();
+        return false;
     }
 
     @Override
     default boolean canGrow(World world, Random random, BlockPos pos, BlockState state) {
-        return true;
+        return false;
     }
 
     @Override
     default void grow(ServerWorld world, Random random, BlockPos pos, BlockState state) {
-        UndergroundConfiguredFeatures.MOSS_PATCH_BONEMEAL.value().generate(world, world.getChunkManager().getChunkGenerator(), random, pos.up());
+        // UndergroundConfiguredFeatures.MOSS_PATCH_BONEMEAL.value().generate(world, world.getChunkManager().getChunkGenerator(), random, pos.up());
         // PileConfiguredFeatures.PILE_HAY.value().generate(world, world.getChunkManager().getChunkGenerator(), random, pos.up());
         // NetherConfiguredFeatures.BASALT_BLOBS.value().generate(world, world.getChunkManager().getChunkGenerator(), random, pos.up());
         // MiscConfiguredFeatures.DESERT_WELL.value().generate(world, world.getChunkManager().getChunkGenerator(), random, pos.up());
