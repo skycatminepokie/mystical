3c380747ddc2b65f5cdcdbcc5b645c95d1afe1a8
Obie Munoz <obiemunozjr@gmail.com>
2023-02-22 23:39:10 -0500

    Update StatCodec.java
    Updated error checking

--- src/main/java/skycat/mystical/util/StatCodec.java
+++ src/main/java/skycat/mystical/util/StatCodec.java
@@ -13,7 +13,7 @@ import net.minecraft.util.registry.Registry;
 
 import java.lang.reflect.Type;
 
-public class StatCodec implements Codec<Stat<?>>, JsonSerializer<Stat<?>>, JsonDeserializer<Stat<?>> { // TODO Use and test
+public class StatCodec implements Codec<Stat<?>>, JsonSerializer<Stat<?>>, JsonDeserializer<Stat<?>> {
     public static StatCodec INSTANCE = new StatCodec();
     public static Codec<Pair<StatType<?>, Identifier>> TYPE_IDENTIFIER_CODEC = Codec.pair(
             Registry.STAT_TYPE.getCodec().fieldOf("type").codec(),
@@ -27,24 +27,29 @@ public class StatCodec implements Codec<Stat<?>>, JsonSerializer<Stat<?>>, JsonD
     public <T,S> Stat<S> getStat(DynamicOps<T> ops, T input) {
         var result = TYPE_IDENTIFIER_CODEC.parse(ops, input);
         if (result.result().isPresent()) {
-            // Success
             try {
-                StatType<S> type = (StatType<S>) result.result().get().getFirst(); // WARN Check cast
+                StatType<S> type = (StatType<S>) result.result().get().getFirst();
                 Stat<S> stat = type.getOrCreateStat(type.getRegistry().get(result.result().get().getSecond()));
                 return stat;
-            } catch (ClassCastException e) {
-                Utils.log("oof");
+            } catch (Exception e) {
+                throw new JsonParseException("Failed to deserialize Stat object: " + e.getMessage());
             }
         } else {
-            Utils.log("other oof");
+            throw new JsonParseException("Failed to deserialize Stat object: invalid input");
         }
-        return null;
     }
 
     @Override
     public <T> DataResult<Pair<Stat<?>, T>> decode(DynamicOps<T> ops, T input) {
-        // In the case of using json, T is JsonElement
-        return DataResult.success(Pair.of(getStat(ops, input),input)); // WARN Error handling is currently awful
+        try {
+            Stat<?> stat = getStat(ops, input);
+            if (stat == null) {
+                throw new JsonParseException("Failed to deserialize Stat object");
+            }
+            return DataResult.success(Pair.of(stat, input));
+        } catch (Exception e) {
+            return DataResult.error("Failed to deserialize Stat object: " + e.getMessage());
+        }
     }
 
     @Override
@@ -54,11 +59,26 @@ public class StatCodec implements Codec<Stat<?>>, JsonSerializer<Stat<?>>, JsonD
 
     @Override
     public Stat<?> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-        return getStat(JsonOps.INSTANCE, json);
+        try {
+            Stat<?> stat = getStat(JsonOps.INSTANCE, json);
+            if (stat == null) {
+                throw new JsonParseException("Failed to deserialize Stat object");
+            }
+            return stat;
+        } catch (Exception e) {
+            throw new JsonParseException("Failed to deserialize Stat object: " + e.getMessage());
+        }
     }
 
     @Override
     public JsonElement serialize(Stat<?> src, Type typeOfSrc, JsonSerializationContext context) {
-        return encodeStart(JsonOps.INSTANCE, src).getOrThrow(false, s -> Utils.log("awful 62 statcodec")); // WARN Error checking
+        try {
+            JsonElement json = genericEncode(src, JsonOps.INSTANCE, JsonNull.INSTANCE).result().orElseThrow(() ->
+                new JsonParseException("Failed to serialize Stat object")
+            );
+            return json;
+        } catch (Exception e) {
+            throw new JsonParseException("Failed to serialize Stat object: " + e.getMessage());
+        }
     }
 }
