023fbad666cd2c7f9088bc3b42933f06d61463d3
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-03-10 19:55:28 -0800

    Fishing rod launch works with players

--- src/main/java/skycat/mystical/mixin/FishingBobberEntityMixin.java
+++ src/main/java/skycat/mystical/mixin/FishingBobberEntityMixin.java
@@ -1,10 +1,16 @@
 package skycat.mystical.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import skycat.mystical.Mystical;
 
 @Mixin(FishingBobberEntity.class)
@@ -15,6 +21,13 @@ public abstract class FishingBobberEntityMixin {
             return velocity.multiply(20.0); // TODO: config
         }
         return velocity;
+    }
+
+    @Inject(method = "pullHookedEntity", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void afterPullHooked(Entity entity, CallbackInfo ci, Entity entity2, Vec3d vec3d) {
+        if (Mystical.SPELL_HANDLER.shouldDoFishingRodLaunch() && entity instanceof ServerPlayerEntity) {
+            ((ServerPlayerEntity) entity).networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(entity));
+        }
     }
 
 
