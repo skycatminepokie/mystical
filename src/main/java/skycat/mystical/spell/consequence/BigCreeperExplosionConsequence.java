c9f2f17d6b1f4c4150cb32a01078461422606f65
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-03-16 17:34:57 -0700

    Remove unnecessary stuff from BigCreeperExplosionConsequence

--- src/main/java/skycat/mystical/spell/consequence/BigCreeperExplosionConsequence.java
+++ src/main/java/skycat/mystical/spell/consequence/BigCreeperExplosionConsequence.java
@@ -8,15 +8,15 @@ import java.util.Random;
 public class BigCreeperExplosionConsequence extends SpellConsequence {
     public static final Factory FACTORY = new Factory();
 
-    private BigCreeperExplosionConsequence(Class consequenceType, Class callbackType) {
-        super(consequenceType, callbackType, "bigCreeperExplosion", "Bigger Creeper Explosions", "Creepers go boom. But more.");
+    private BigCreeperExplosionConsequence() {
+        super(BigCreeperExplosionConsequence.class, BigCreeperExplosionConsequence.class, "bigCreeperExplosion", "Bigger Creeper Explosions", "Creepers go boom. But more.");
     }
 
     public static class Factory implements ConsequenceFactory<BigCreeperExplosionConsequence> {
         @NotNull
         @Override
         public BigCreeperExplosionConsequence make(@NonNull Random random, double points) {
-            return new BigCreeperExplosionConsequence(BigCreeperExplosionConsequence.class, BigCreeperExplosionConsequence.class);
+            return new BigCreeperExplosionConsequence();
         }
     }
 }
