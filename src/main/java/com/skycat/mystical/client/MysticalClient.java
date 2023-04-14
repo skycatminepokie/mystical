f53f6160a175e84b86d9fd0583e283f271b71f9a
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-04-13 19:43:57 -0700

    Fixes #77

--- src/main/java/com/skycat/mystical/client/MysticalClient.java
+++ src/main/java/com/skycat/mystical/client/MysticalClient.java
@@ -2,11 +2,19 @@ package com.skycat.mystical.client;
 
 import com.skycat.mystical.client.command.MysticalClientCommandHandler;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.text.Text;
 
 public class MysticalClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientCommandRegistrationCallback.EVENT.register(new MysticalClientCommandHandler());
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+            dispatcher.register(ClientCommandManager.literal("hello").executes(context -> {
+                context.getSource().sendFeedback(Text.literal("Hello, world!"));
+                return 0;
+            }));
+        });
     }
 }
--- src/main/java/com/skycat/mystical/client/command/MysticalClientCommandHandler.java
+++ src/main/java/com/skycat/mystical/client/command/MysticalClientCommandHandler.java
@@ -2,19 +2,20 @@ package com.skycat.mystical.client.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.command.CommandRegistryAccess;
 
 public class MysticalClientCommandHandler implements ClientCommandRegistrationCallback {
+
     @Override
     public void register(CommandDispatcher<FabricClientCommandSource> dispatcher, CommandRegistryAccess registryAccess) {
-
+        dispatcher.register(ClientCommandManager.literal("mystConf").executes(this::openConfig));
     }
 
-    private int openConfig(CommandContext<Object> objectCommandContext) {
-        return 0;
+    private int openConfig(CommandContext<FabricClientCommandSource> context) {
+
+        return 1;
     }
-
-
 }
