60cdd64f75624c6fd7c93c8a8bbcc6a547a6b174
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-04-24 08:33:26 -0700

    addPower/getPower

--- src/main/java/com/skycat/mystical/server/HavenManager.java
+++ src/main/java/com/skycat/mystical/server/HavenManager.java
@@ -4,17 +4,21 @@ import com.skycat.mystical.Mystical;
 import com.skycat.mystical.common.util.Utils;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.util.UUID;
 
 public class HavenManager {
     public HashSet<ChunkPos> havenedChunks = new HashSet<>();
+    public HashMap<UUID, Integer> powerMap = new HashMap<>();
     private static final File SAVE_FILE = new File("config/havenManager.json");
     public static HavenManager loadOrNew() {
         try (Scanner scanner = new Scanner(SAVE_FILE)) {
@@ -136,5 +140,54 @@ public class HavenManager {
 
     public boolean isInHaven(Entity entity) {
         return isInHaven(entity.getChunkPos());
+    }
+
+    /**
+     * Gives power to a player.
+     * @param player The player to grant power to.
+     * @param power The amount of power to grant.
+     * @return The amount of power the player has after adding.
+     */
+    public int addPower(ServerPlayerEntity player, int power) {
+        return addPower(player.getUuid(), power);
+    }
+
+    /**
+     * Gives power to a player.
+     * @param playerUUID The UUID of a player.
+     * @param power The amount of power to grant.
+     * @return The total power the player has after adding power.
+     */
+    public int addPower(UUID playerUUID, int power) {
+        Integer currentPower = powerMap.get(playerUUID);
+        if (currentPower != null) { // I believe this implies powerMap.containsKey(playerUUID)
+            int sum = currentPower + power;
+            powerMap.put(playerUUID, sum);
+            return sum;
+        }
+        // else
+        powerMap.put(playerUUID, power);
+        return power;
+    }
+
+    /**
+     * Get how much power a player has.
+     * @param playerUUID The UUID of the player.
+     * @return The power a player has, or 0 if they have no power/power is not yet tracked for this player.
+     */
+    public int getPower(UUID playerUUID) {
+        if (powerMap.get(playerUUID) == null) {
+            return 0;
+        }
+        return powerMap.get(playerUUID);
+    }
+
+    /**
+     * Get how much power a player has.
+     * @param player The player.
+     * @return The power the player has, or 0 if they have no power/power is not yet tracked for this player.
+     */
+    public int getPower(ServerPlayerEntity player) {
+        return getPower(player.getUuid());
     }
 }
