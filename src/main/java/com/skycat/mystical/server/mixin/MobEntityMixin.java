69284d79f51ed200bbcdc934a63088e759f92413
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-04-29 22:10:40 -0700

    disableDaylightBurning no longer works in havens

--- src/main/java/com/skycat/mystical/server/mixin/MobEntityMixin.java
+++ src/main/java/com/skycat/mystical/server/mixin/MobEntityMixin.java
@@ -3,16 +3,18 @@ package com.skycat.mystical.server.mixin;
 import com.skycat.mystical.Mystical;
 import com.skycat.mystical.common.spell.consequence.DisableDaylightBurningConsequence;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.world.entity.EntityLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MobEntity.class)
-public abstract class MobEntityMixin {
+public abstract class MobEntityMixin implements EntityLike {
     @Inject(method = "isAffectedByDaylight", at = @At("HEAD"), cancellable = true)
     private void cancelDaylightEffects(CallbackInfoReturnable<Boolean> cir) {
-        if (Mystical.SPELL_HANDLER.isConsequenceActive(DisableDaylightBurningConsequence.class)) { // TODO allow for havening
+        if (!Mystical.HAVEN_MANAGER.isInHaven(getBlockPos()) &&
+                Mystical.SPELL_HANDLER.isConsequenceActive(DisableDaylightBurningConsequence.class)) { // TODO allow for havening
             cir.setReturnValue(false);
         }
     }
--- src/main/java/com/skycat/mystical/server/mixin/SheepEntityMixin.java
+++ src/main/java/com/skycat/mystical/server/mixin/SheepEntityMixin.java
@@ -6,6 +6,7 @@ import com.skycat.mystical.common.util.Utils;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.util.DyeColor;
 import net.minecraft.util.Util;
+import net.minecraft.world.entity.EntityLike;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,12 +14,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SheepEntity.class)
-public abstract class SheepEntityMixin {
+public abstract class SheepEntityMixin implements EntityLike {
     @Shadow public abstract void setColor(DyeColor color);
 
     @Inject(method = "onEatingGrass", at = @At("TAIL"))
     public void afterEatGrass(CallbackInfo ci) {
-        if (Mystical.HAVEN_MANAGER.isInHaven((SheepEntity) (Object) this) &&
+        // https://www.reddit.com/r/fabricmc/comments/kfrsq7/comment/ggactl1/
+        if (Mystical.HAVEN_MANAGER.isInHaven(getBlockPos()) &&
                 Mystical.SPELL_HANDLER.isConsequenceActive(SheepColorChangeConsequence.class) && Utils.percentChance(Mystical.CONFIG.sheepColorChange.chance())) {
             setColor(Util.getRandom(DyeColor.values(), Mystical.MC_RANDOM));
             Utils.log(Utils.translateString("text.mystical.consequence.sheepColorChange.fired"), Mystical.CONFIG.sheepColorChange.logLevel());
