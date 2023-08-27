d3cfb1291f79a9b90da0446e563a2fac11c05bfd
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-08-26 22:46:51 -0700

    Wrote datagen stuff

--- src/main/java/com/skycat/mystical/datagen/DataGenerator.java
+++ src/main/java/com/skycat/mystical/datagen/DataGenerator.java
@@ -6,16 +6,24 @@ import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.Identifier;
 
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 public class DataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         var pack = fabricDataGenerator.createPack();
         pack.addProvider(EnglishLangProvider::new);
+        pack.addProvider(EntityTypeTagProvider::new);
     }
     private static class EnglishLangProvider extends FabricLanguageProvider {
 
@@ -136,5 +144,39 @@ public class DataGenerator implements DataGeneratorEntrypoint {
             tb.add("text.mystical.command." + key, value);
         }
 
+    }
+    private static class EntityTypeTagProvider extends FabricTagProvider.EntityTypeTagProvider {
+        private static final TagKey<EntityType<?>> BOSSES = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier("mystical:bosses"));
+        // Using tags like this will make startup time a bit longer, but will allow for compatibility
+        private static final TagKey<EntityType<?>> ZOMBIE_VARIANTS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier("mystical:zombie_variants"));
+        private static final TagKey<EntityType<?>> SKELETON_VARIANTS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier("mystical:skeleton_variants"));
+        private static final TagKey<EntityType<?>> ENDERMAN_VARIANTS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier("mystical:enderman_variants"));
+
+        public EntityTypeTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+            super(output, completableFuture);
+        }
+
+        @Override
+        protected void configure(RegistryWrapper.WrapperLookup arg) {
+            getOrCreateTagBuilder(BOSSES)
+                    .add(EntityType.ENDER_DRAGON)
+                    .add(EntityType.WITHER)
+                    .add(EntityType.WARDEN)
+                    .add(EntityType.ELDER_GUARDIAN)
+                    .addOptionalTag(new Identifier("c:bosses"));
+            getOrCreateTagBuilder(ZOMBIE_VARIANTS)
+                    .add(EntityType.ZOMBIE)
+                    .add(EntityType.HUSK)
+                    .add(EntityType.ZOMBIE_VILLAGER)
+                    .add(EntityType.ZOMBIFIED_PIGLIN)
+                    .add(EntityType.DROWNED);
+            getOrCreateTagBuilder(SKELETON_VARIANTS)
+                    .add(EntityType.SKELETON)
+                    .add(EntityType.WITHER_SKELETON)
+                    .add(EntityType.STRAY);
+            getOrCreateTagBuilder(ENDERMAN_VARIANTS)
+                    .add(EntityType.ENDERMAN)
+                    .add(EntityType.ENDERMITE);
+        }
     }
 }
