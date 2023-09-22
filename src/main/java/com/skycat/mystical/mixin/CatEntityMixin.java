529a8d7f985d35b8b206dea593cf5853432f0d91
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-09-22 16:09:35 -0700

    CatEntityEvents.Eat

--- src/main/java/com/skycat/mystical/mixin/CatEntityMixin.java
+++ src/main/java/com/skycat/mystical/mixin/CatEntityMixin.java
@@ -1,41 +1,22 @@
 package com.skycat.mystical.mixin;
 
-import com.skycat.mystical.Mystical;
-import com.skycat.mystical.common.spell.consequence.CatVariantChangeConsequence;
-import com.skycat.mystical.common.util.Utils;
+import com.skycat.mystical.event.CatEntityEvents;
 import net.minecraft.entity.passive.CatEntity;
-import net.minecraft.entity.passive.CatVariant;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
 import net.minecraft.util.Hand;
-import net.minecraft.util.Util;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.ArrayList;
-
 @Mixin(CatEntity.class)
-public abstract class CatEntityMixin { // I could probably turn this into an event
-    @Unique private static final ArrayList<CatVariant> VARIANTS = new ArrayList<>();
-    @Shadow public abstract void setVariant(CatVariant variant);
-
+public abstract class CatEntityMixin {
+    /**
+     * Fire the {@link CatEntityEvents.Eat} event.
+     */
     @Inject(method = "eat", at = @At("HEAD"))
     public void eat(PlayerEntity entity, Hand hand, ItemStack stack, CallbackInfo ci) {
-        if (VARIANTS.isEmpty()) { // Should this be in static? Seems like delaying initialization might be good though.
-            for (CatVariant catVariant : Registries.CAT_VARIANT) {
-                VARIANTS.add(catVariant);
-            }
-        }
-        if ((Mystical.isClientWorld() && Mystical.getSpellHandler().isConsequenceActive(CatVariantChangeConsequence.class)) &&
-                !(Mystical.isClientWorld() && Mystical.getHavenManager().isInHaven(entity)) &&
-                Utils.percentChance(Mystical.CONFIG.catVariantChange.chance())) {
-            setVariant(Util.getRandom(VARIANTS, Mystical.MC_RANDOM));
-            Utils.log(Utils.translateString("text.mystical.consequence.catVariantChange.fired"), Mystical.CONFIG.catVariantChange.logLevel());
-        }
+        CatEntityEvents.EAT.invoker().onEat((CatEntity) (Object) this, entity, hand, stack);
     }
 }
