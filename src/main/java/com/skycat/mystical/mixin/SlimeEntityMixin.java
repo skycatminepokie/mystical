f956d1b14fe2fd0e475e250e95100f892c007f8b
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2023-12-26 15:54:35 -0800

    Maybe work untested

---
+++ src/main/java/com/skycat/mystical/mixin/SlimeEntityMixin.java
@@ -0,0 +1,17 @@
+package com.skycat.mystical.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(SlimeEntity.class)
+public class SlimeEntityMixin {
+    @WrapOperation(method = "canSpawn(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/random/Random;)Z",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
+    private static int modifyIsSlimeChunk(Random random, int bound, Operation<Integer> original) {
+        return 0;
+    }
+}
