14760d94de2911e1a6be08f2d677e86695541972
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-06 16:23:30 -0800

    Mixin finished

--- src/main/java/com/skycat/mystical/mixin/SlimeEntityMixin.java
+++ src/main/java/com/skycat/mystical/mixin/SlimeEntityMixin.java
@@ -1,17 +1,20 @@
 package com.skycat.mystical.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.WorldAccess;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SlimeEntity.class)
 public class SlimeEntityMixin {
-    @WrapOperation(method = "canSpawn(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/random/Random;)Z",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/random/Random;nextInt(I)I"))
-    private static int modifyIsSlimeChunk(Random random, int bound, Operation<Integer> original) {
-        return 0;
+    @Inject(method = "canSpawn(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/WorldAccess;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/util/math/random/Random;)Z", at = @At(value = "HEAD", shift = At.Shift.AFTER), cancellable = true)
+    private static void modifySlimeSpawning(EntityType<SlimeEntity> type, WorldAccess world, SpawnReason spawnReason, BlockPos pos, Random random, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(SlimeEntity.canMobSpawn(type, world, spawnReason, pos, random));
     }
 }
