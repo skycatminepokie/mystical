a313683093549985f61b1e9ed2acc4808c151a38
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-08 15:07:41 -0800

    Closes #6

---
+++ src/main/java/com/skycat/mystical/mixin/TimeCommandMixin.java
@@ -0,0 +1,22 @@
+package com.skycat.mystical.mixin;
+
+import com.skycat.mystical.Mystical;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.TimeCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TimeCommand.class)
+public abstract class TimeCommandMixin {
+    @Inject(method = "executeSet", at = @At("HEAD"))
+    private static void onTimeSetCommand(ServerCommandSource source, int time, CallbackInfoReturnable<Integer> cir) {
+        Mystical.getEVENT_HANDLER().setNightTimer();
+    }
+
+    @Inject(method = "executeAdd", at = @At("HEAD"))
+    private static void onTimeAddCommand(ServerCommandSource source, int time, CallbackInfoReturnable<Integer> cir) {
+        Mystical.getEVENT_HANDLER().setNightTimer();
+    }
+}
