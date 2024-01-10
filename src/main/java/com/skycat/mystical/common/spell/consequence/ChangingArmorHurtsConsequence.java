6145a9f76281fbfd1396c3f28958eeaf12bd37c2
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-01-09 16:09:41 -0800

    Standardize formatting some

--- src/main/java/com/skycat/mystical/common/spell/consequence/ChangingArmorHurtsConsequence.java
+++ src/main/java/com/skycat/mystical/common/spell/consequence/ChangingArmorHurtsConsequence.java
@@ -6,16 +6,17 @@ import lombok.NonNull;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
-
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
+
 import static net.minecraft.entity.damage.DamageTypes.THORNS;
 
 public class ChangingArmorHurtsConsequence extends SpellConsequence implements ServerEntityEvents.EquipmentChange {
     public static final Factory FACTORY = new Factory();
+
     public ChangingArmorHurtsConsequence() {
         super(ChangingArmorHurtsConsequence.class, ServerEntityEvents.EquipmentChange.class, 10D);
     }
@@ -27,12 +28,11 @@ public class ChangingArmorHurtsConsequence extends SpellConsequence implements S
 
     @Override
     public void onChange(LivingEntity livingEntity, EquipmentSlot equipmentSlot, ItemStack previousStack, ItemStack currentStack) {
-        if(livingEntity instanceof ServerPlayerEntity Player && Utils.percentChance(10d) && equipmentSlot.isArmorSlot())  {
+        if (livingEntity instanceof ServerPlayerEntity Player && Utils.percentChance(10d) && equipmentSlot.isArmorSlot()) {
             Player.damage(Player.getServerWorld().getDamageSources().create(THORNS), 1);
         }
-
-
     }
+
     public static class Factory extends ConsequenceFactory<ChangingArmorHurtsConsequence> {
         protected Factory() {
             super("reverse_thorns",
@@ -44,13 +44,13 @@ public class ChangingArmorHurtsConsequence extends SpellConsequence implements S
         }
 
         @Override
-        public @NotNull ChangingArmorHurtsConsequence make(@NonNull Random random, double points) {
-            return new ChangingArmorHurtsConsequence();
+        public double getWeight() {
+            return 0;
         }
 
         @Override
-        public double getWeight() {
-            return 0;
+        public @NotNull ChangingArmorHurtsConsequence make(@NonNull Random random, double points) {
+            return new ChangingArmorHurtsConsequence();
         }
     }
 
