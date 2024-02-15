d24437d89cb96f5b53b56839c52eee34563f49b7
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-02-14 21:34:41 -0800

    Split sources
    Using template generator, https://fabricmc.net/develop/template/
    
    Signed-off-by: skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>

---
+++ src/client/java/com/skycat/mystical/MysticalClient.java
@@ -0,0 +1,9 @@
+package com.skycat.mystical;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MysticalClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
