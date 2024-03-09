25c00eea1f6c5132837052c9c6a3ec959b634741
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-03-08 16:59:44 -0800

    Maybe fix double nighttime events

--- src/main/java/com/skycat/mystical/mixin/ServerWorldMixin.java
+++ src/main/java/com/skycat/mystical/mixin/ServerWorldMixin.java
@@ -18,7 +18,7 @@ public abstract class ServerWorldMixin {
     @Shadow @Final private ServerWorldProperties worldProperties;
     @Shadow public abstract @NotNull MinecraftServer getServer();
 
-    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V", shift = At.Shift.BEFORE))
+    @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V", shift = At.Shift.AFTER))
     private void beforeSkippingNight(CallbackInfo ci) {
         if (worldProperties.getTimeOfDay() % 24000 <= MysticalEventHandler.NIGHT_TIME) { // If the time to do night things hasn't passed, do them now
             Mystical.EVENT_HANDLER.doNighttimeEvents(getServer());
