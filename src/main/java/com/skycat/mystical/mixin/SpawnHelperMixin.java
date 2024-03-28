8ab0763977706148c491083cdb30711270db604b
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-03-28 13:41:18 -0700

    Removed a crud ton of locals with @Local

--- src/main/java/com/skycat/mystical/mixin/SpawnHelperMixin.java
+++ src/main/java/com/skycat/mystical/mixin/SpawnHelperMixin.java
@@ -1,43 +1,31 @@
 package com.skycat.mystical.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.skycat.mystical.Mystical;
 import com.skycat.mystical.spell.Spell;
 import com.skycat.mystical.spell.consequence.MobSpawnSwapConsequence;
 import com.skycat.mystical.spell.consequence.TurboMobsConsequence;
 import com.skycat.mystical.util.Utils;
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.EntityData;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.SpawnHelper;
-import net.minecraft.world.biome.SpawnSettings;
-import net.minecraft.world.chunk.Chunk;
-import net.minecraft.world.gen.StructureAccessor;
-import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(SpawnHelper.class)
 public abstract class SpawnHelperMixin {
     @Inject(method = "spawnEntitiesInChunk(Lnet/minecraft/entity/SpawnGroup;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/world/chunk/Chunk;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/SpawnHelper$Checker;Lnet/minecraft/world/SpawnHelper$Runner;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V", shift = At.Shift.AFTER),
-            locals = LocalCapture.CAPTURE_FAILSOFT)
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V", shift = At.Shift.AFTER))
     // Tried using ModifyVariable, but locations weren't set up yet. // TY llamalad7, benonardo
-    private static void mystical_giveSpeedBoost(SpawnGroup group, ServerWorld world, Chunk chunk, BlockPos pos, SpawnHelper.Checker checker, SpawnHelper.Runner runner, CallbackInfo ci, StructureAccessor structureAccessor, ChunkGenerator chunkGenerator, int i, BlockState blockState, BlockPos.Mutable mutable, int j, int k, int l, int m, int n, SpawnSettings.SpawnEntry spawnEntry, EntityData entityData, int o, int p, int q, double d, double e, PlayerEntity playerEntity, double f, MobEntity mobEntity) {
+    private static void mystical_giveSpeedBoost(CallbackInfo ci, @Local(ordinal = 0) MobEntity mobEntity) {
         // Most of the parameters are unused :(
-        if (Mystical.isClientWorld()) return;
+        if (Mystical.isClientWorld()) return; // Probably won't happen, but I'm leaving it just cuz.
         for (Spell spell : Mystical.getSpellHandler().spellsOfConsequenceType(TurboMobsConsequence.class)) { // Inside this, there must be an active TurboMobsConsequence spell
             if (!Mystical.getHavenManager().isInHaven(mobEntity) &&
                     ((TurboMobsConsequence) spell.getConsequence()).entityType.equals(mobEntity.getType())) { // Make sure it applies to this type // TODO: Chance
