f76d538354c0009b77a375cc60bdc282db71df51
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-03-28 16:27:05 -0700

    Combined FishingBobberEntityMixin into one mixin

--- src/main/java/com/skycat/mystical/mixin/FishingBobberEntityMixin.java
+++ src/main/java/com/skycat/mystical/mixin/FishingBobberEntityMixin.java
@@ -12,60 +12,41 @@ import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.network.packet.s2c.play.EntityVelocityUpdateS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(FishingBobberEntity.class)
 public abstract class FishingBobberEntityMixin implements Ownable {
 
-    @Shadow
-    @Nullable
-    public abstract Entity getHookedEntity();
-
-    @Inject(method = "pullHookedEntity", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void mystical_updateForLaunchSpell(Entity entity, CallbackInfo ci, Entity entity2, Vec3d vec3d) {
-        if (!Mystical.isClientWorld() &&
-                !Mystical.getHavenManager().isInHaven(entity) &&
-                !Mystical.getHavenManager().isInHaven(entity2) &&
-                entity instanceof ServerPlayerEntity &&
-                Mystical.getSpellHandler().isConsequenceActive(FishingRodLaunchConsequence.class)) {
-            ((ServerPlayerEntity) entity).networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(entity)); // Thanks @Wesley1808#9858 :)
-        }
-    }
-
-    @ModifyArg(method = "pullHookedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"), index = 0)
-    public Vec3d mystical_handleLaunchSpell(Vec3d velocity) {
-        Entity hookedEntity = getHookedEntity();
-        if (hookedEntity != null &&
-                !Mystical.isClientWorld() &&
-                !Mystical.getHavenManager().isInHaven(hookedEntity) &&
-                Mystical.getSpellHandler().isConsequenceActive(FishingRodLaunchConsequence.class) &&
-                Utils.percentChance(Mystical.CONFIG.fishingRodLaunch.chance())) {
-            Utils.log(Utils.translateString("text.mystical.consequence.fishingRodLaunch.fired"), Mystical.CONFIG.fishingRodLaunch.logLevel());
-            return velocity.multiply(Mystical.CONFIG.fishingRodLaunch.multiplier());
-        }
-        return velocity;
-    }
-
     @WrapOperation(method = "pullHookedEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;setVelocity(Lnet/minecraft/util/math/Vec3d;)V"))
-    private void mystical_swapPulledEntity(Entity hooked, Vec3d velocity, Operation<Void> original) {
+    private void mystical_managePullHookedEntitySpells(Entity hooked, Vec3d velocity, Operation<Void> original) {
         Entity owner = getOwner();
         assert owner != null; // Checked by vanilla
-        if (Mystical.isClientWorld() ||
-                !Utils.percentChance(Mystical.CONFIG.fishingRodSwap.chance()) ||
-                Mystical.getHavenManager().isInHaven(owner) ||
-                !Mystical.getSpellHandler().isConsequenceActive(FishingRodSwapConsequence.class)) {
+        if (Mystical.isClientWorld()) {
             original.call(hooked, velocity);
             return;
         }
 
-        owner.setVelocity(owner.getVelocity().add(velocity.multiply(-1)));
+        int velocityMultiplier = 1;
+        if (!Mystical.getHavenManager().isInHaven(owner) &&
+                Mystical.getSpellHandler().isConsequenceActive(FishingRodSwapConsequence.class) &&
+                Utils.percentChance(Mystical.CONFIG.fishingRodSwap.chance())) {
+            velocityMultiplier *= -1; // fishingRodSwap
+        }
+
+        if (!Mystical.getHavenManager().isInHaven(hooked) &&
+        Mystical.getSpellHandler().isConsequenceActive(FishingRodLaunchConsequence.class) &&
+        Utils.percentChance(Mystical.CONFIG.fishingRodLaunch.chance())) {
+            velocityMultiplier *= Mystical.CONFIG.fishingRodLaunch.multiplier();
+        }
+
+        if (velocityMultiplier != 1) { // If neither consequence activated, might as well keep compatibility in mind.
+            original.call(hooked, velocity);
+            return;
+        }
+
+        owner.setVelocity(owner.getVelocity().add(velocity.multiply(velocityMultiplier)));
+
         if (owner instanceof ServerPlayerEntity player) {
             player.networkHandler.sendPacket(new EntityVelocityUpdateS2CPacket(player)); // Thanks @Wesley1808#9858 :)
         }
