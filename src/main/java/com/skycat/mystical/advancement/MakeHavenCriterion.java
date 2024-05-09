d116627cbca33f0135dcd4fb58c162b0dd36f9ed
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-05-08 19:15:02 -0700

    Fixed MakeHavenCriterion

--- src/main/java/com/skycat/mystical/advancement/MakeHavenCriterion.java
+++ src/main/java/com/skycat/mystical/advancement/MakeHavenCriterion.java
@@ -1,23 +1,22 @@
 package com.skycat.mystical.advancement;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
 import net.minecraft.advancement.criterion.AbstractCriterion;
-import net.minecraft.advancement.criterion.AbstractCriterionConditions;
-import net.minecraft.predicate.entity.AdvancementEntityPredicateDeserializer;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.dynamic.Codecs;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 import java.util.Optional;
 
 public class MakeHavenCriterion extends AbstractCriterion<MakeHavenCriterion.Conditions> {
-    public static final @NotNull Identifier ID = Objects.requireNonNull(Identifier.of("mystical", "make_haven")); // TODO: Figure out where this goes
+    public static final @NotNull Identifier ID = Objects.requireNonNull(Identifier.of("mystical", "make_haven"));
 
     @Override
-    protected com.skycat.mystical.advancement.MakeHavenCriterion.Conditions conditionsFromJson(JsonObject obj, Optional<LootContextPredicate> playerPredicate, AdvancementEntityPredicateDeserializer predicateDeserializer) {
-        return new com.skycat.mystical.advancement.MakeHavenCriterion.Conditions(playerPredicate);
+    public Codec<Conditions> getConditionsCodec() {
+        return Conditions.CODEC;
     }
 
     public Identifier getId() {
@@ -28,10 +27,17 @@ public class MakeHavenCriterion extends AbstractCriterion<MakeHavenCriterion.Con
         super.trigger(player, com.skycat.mystical.advancement.MakeHavenCriterion.Conditions::requirementsMet);
     }
 
-    public static class Conditions extends AbstractCriterionConditions {
-        @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+    public static class Conditions implements AbstractCriterion.Conditions {
+        public static final Codec<Conditions> CODEC = Codecs.createStrictOptionalFieldCodec(LootContextPredicate.CODEC, "player").xmap(Conditions::new, Conditions::player).codec();
+         protected Optional<LootContextPredicate> playerPredicate;
         public Conditions(Optional<LootContextPredicate> playerPredicate) {
-            super(playerPredicate);
+            this.playerPredicate = playerPredicate;
+        }
+
+        @Override
+        public Optional<LootContextPredicate> player() {
+            return playerPredicate;
         }
 
         @SuppressWarnings("SameReturnValue")
