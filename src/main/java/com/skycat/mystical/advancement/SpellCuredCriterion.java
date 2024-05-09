88975a1a97cca00340e0d703d808e4d63581ac90
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-05-08 19:35:11 -0700

    Fixed SpellCuredCriterion

--- src/main/java/com/skycat/mystical/advancement/SpellCuredCriterion.java
+++ src/main/java/com/skycat/mystical/advancement/SpellCuredCriterion.java
@@ -1,16 +1,16 @@
 package com.skycat.mystical.advancement;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.skycat.mystical.spell.Spell;
 import com.skycat.mystical.spell.cure.SpellCure;
 import net.minecraft.advancement.criterion.AbstractCriterion;
-import net.minecraft.advancement.criterion.AbstractCriterionConditions;
 import net.minecraft.predicate.NumberRange;
-import net.minecraft.predicate.entity.AdvancementEntityPredicateDeserializer;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -19,10 +19,8 @@ public class SpellCuredCriterion extends AbstractCriterion<SpellCuredCriterion.C
     public static final @NotNull Identifier ID = Objects.requireNonNull(Identifier.of("mystical", "spell_cured")); // TODO: Figure out where this goes
 
     @Override
-    protected com.skycat.mystical.advancement.SpellCuredCriterion.Conditions conditionsFromJson(JsonObject obj, Optional<LootContextPredicate> playerPredicate, AdvancementEntityPredicateDeserializer predicateDeserializer) {
-        NumberRange.DoubleRange contributionPercentage = NumberRange.DoubleRange.fromJson(obj.get("contributionPercentage")); // Ends up with NumberRange.FloatRange.ANY if obj.get(...) returns null.
-        NumberRange.IntRange participants = NumberRange.IntRange.fromJson(obj.get("participants")); // Ends up with NumberRange.IntRange.ANY if obj.get(...) returns null.
-        return new com.skycat.mystical.advancement.SpellCuredCriterion.Conditions(playerPredicate, contributionPercentage, participants);
+    public Codec<Conditions> getConditionsCodec() {
+        return Conditions.CODEC;
     }
 
     public Identifier getId() {
@@ -33,32 +31,47 @@ public class SpellCuredCriterion extends AbstractCriterion<SpellCuredCriterion.C
         super.trigger(player, conditions -> conditions.requirementsMet(player, spell));
     }
 
-    public static class Conditions extends AbstractCriterionConditions { // TODO: Turn this into a full-fledged matching system?
-        protected final NumberRange.DoubleRange contributionPercentage;
-        protected final NumberRange.IntRange participants;
-
-        public Conditions(Optional<LootContextPredicate> entity, NumberRange.DoubleRange contributionPercentage) {
-            this(entity, contributionPercentage, NumberRange.IntRange.ANY);
+    public static class Conditions implements AbstractCriterion.Conditions { // TODO: Turn this into a full-fledged matching system?
+        public static final Codec<Conditions> CODEC = RecordCodecBuilder.create(instance -> (instance.group(
+                LootContextPredicate.CODEC.optionalFieldOf("player").forGetter(Conditions::player),
+                NumberRange.DoubleRange.CODEC.optionalFieldOf("contributionPercentage").forGetter(Conditions::getContributionPercentage),
+                NumberRange.IntRange.CODEC.optionalFieldOf("participants").forGetter(Conditions::getParticipants)
+        ).apply(instance, Conditions::new)));
+        protected final @Nullable NumberRange.DoubleRange contributionPercentage;
+        protected final @Nullable NumberRange.IntRange participants;
+        protected final @Nullable LootContextPredicate player;
+        @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+        public Conditions(Optional<LootContextPredicate> player, Optional<NumberRange.DoubleRange> contributionPercentage, Optional<NumberRange.IntRange> participants) {
+            this.contributionPercentage = contributionPercentage.orElse(null);
+            this.participants = participants.orElse(null);
+            this.player = player.orElse(null);
         }
-
-        public Conditions(Optional<LootContextPredicate> entity, NumberRange.DoubleRange contributionPercentage, NumberRange.IntRange participants) {
-            super(entity);
+        public Conditions(@Nullable LootContextPredicate player, @Nullable NumberRange.DoubleRange contributionPercentage, @Nullable NumberRange.IntRange participants) {
             this.contributionPercentage = contributionPercentage;
             this.participants = participants;
+            this.player = player;
+        }
+
+        public Optional<NumberRange.DoubleRange> getContributionPercentage() {
+            return Optional.ofNullable(contributionPercentage);
+        }
+
+        public Optional<NumberRange.IntRange> getParticipants() {
+            return Optional.ofNullable(participants);
+        }
+
+        @Override
+        public Optional<LootContextPredicate> player() {
+            return Optional.ofNullable(player);
         }
 
         public boolean requirementsMet(ServerPlayerEntity player, Spell spell) {
             SpellCure cure = spell.getCure();
-            return contributionPercentage.test(((double) cure.getContributionsOf(player.getUuid())/cure.getContributionGoal()) * 100) &&
-                    participants.test(cure.getContributorCount());
+            // Return true iff (participants is null OR succeeds) AND (contributionPercentage is null OR succeeds). In other words, a null requirement always succeeds.
+            return (participants == null || participants.test(cure.getContributorCount())) &&
+                    (contributionPercentage == null || contributionPercentage.test(((double) cure.getContributionsOf(player.getUuid()) / cure.getContributionGoal()) * 100));
+
         }
 
-        @Override
-        public JsonObject toJson() {
-            JsonObject json = super.toJson();
-            json.add("contributionPercentage", contributionPercentage.toJson());
-            json.add("participants", participants.toJson());
-            return json;
-        }
     }
 }
