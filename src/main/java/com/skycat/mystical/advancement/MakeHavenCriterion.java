51eb276185eb26922ef8662d1c5cfd742517c72d
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-05-11 14:17:01 -0700

    Fix unnecessary qualifiers

--- src/main/java/com/skycat/mystical/advancement/MakeHavenCriterion.java
+++ src/main/java/com/skycat/mystical/advancement/MakeHavenCriterion.java
@@ -5,7 +5,6 @@ import net.minecraft.advancement.criterion.AbstractCriterion;
 import net.minecraft.predicate.entity.LootContextPredicate;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.dynamic.Codecs;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
@@ -15,8 +14,8 @@ public class MakeHavenCriterion extends AbstractCriterion<MakeHavenCriterion.Con
     public static final @NotNull Identifier ID = Objects.requireNonNull(Identifier.of("mystical", "make_haven"));
 
     @Override
-    public Codec<com.skycat.mystical.advancement.MakeHavenCriterion.Conditions> getConditionsCodec() {
-        return com.skycat.mystical.advancement.MakeHavenCriterion.Conditions.CODEC;
+    public Codec<MakeHavenCriterion.Conditions> getConditionsCodec() {
+        return MakeHavenCriterion.Conditions.CODEC;
     }
 
     public Identifier getId() {
@@ -24,12 +23,12 @@ public class MakeHavenCriterion extends AbstractCriterion<MakeHavenCriterion.Con
     }
 
     public void trigger(ServerPlayerEntity player) {
-        super.trigger(player, com.skycat.mystical.advancement.MakeHavenCriterion.Conditions::requirementsMet);
+        super.trigger(player, MakeHavenCriterion.Conditions::requirementsMet);
     }
 
     @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
     public static class Conditions implements AbstractCriterion.Conditions {
-        public static final Codec<com.skycat.mystical.advancement.MakeHavenCriterion.Conditions> CODEC = Codecs.createStrictOptionalFieldCodec(LootContextPredicate.CODEC, "player").xmap(com.skycat.mystical.advancement.MakeHavenCriterion.Conditions::new, com.skycat.mystical.advancement.MakeHavenCriterion.Conditions::player).codec();
+        public static final Codec<MakeHavenCriterion.Conditions> CODEC = LootContextPredicate.CODEC.optionalFieldOf("player").xmap(Conditions::new, Conditions::player).codec();
          protected Optional<LootContextPredicate> playerPredicate;
         public Conditions(Optional<LootContextPredicate> playerPredicate) {
             this.playerPredicate = playerPredicate;
