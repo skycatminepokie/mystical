4b85f77c00be24f0bf13a51eb886988a77183b84
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-05-11 15:42:49 -0700

    Fix SaveState
    No DFU :(

--- src/main/java/com/skycat/mystical/SaveState.java
+++ src/main/java/com/skycat/mystical/SaveState.java
@@ -1,13 +1,14 @@
 package com.skycat.mystical;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.skycat.mystical.spell.SpellHandler;
 import com.skycat.mystical.util.Utils;
 import lombok.Getter;
+import net.minecraft.datafixer.DataFixTypes;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtOps;
+import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.PersistentState;
 import net.minecraft.world.PersistentStateManager;
@@ -19,7 +20,7 @@ public class SaveState extends PersistentState {
             HavenManager.CODEC.fieldOf("havenManager").forGetter(SaveState::getHavenManager),
             SpellHandler.CODEC.fieldOf("spellHandler").forGetter(SaveState::getSpellHandler)
     ).apply(instance, SaveState::new));
-    public static final Type<SaveState> TYPE = new Type<>(SaveState::new, SaveState::readFromNbt, null);
+    public static final Type<SaveState> TYPE = new Type<>(SaveState::new, SaveState::readFromNbt, DataFixTypes.LEVEL);
 
     public SaveState() {
         this(new HavenManager(), new SpellHandler());
@@ -30,8 +31,8 @@ public class SaveState extends PersistentState {
         this.spellHandler = spellHandler;
     }
     @Override
-    public NbtCompound writeNbt(NbtCompound nbt) {
-        nbt.put("mystical_save", CODEC.encode(this, NbtOps.INSTANCE, NbtOps.INSTANCE.empty()).getOrThrow(false, s -> Utils.log("Failed to save mystical data.")));
+    public NbtCompound writeNbt(NbtCompound nbt, RegistryWrapper.WrapperLookup wrapperLookup) {
+        nbt.put("mystical_save", CODEC.encode(this, NbtOps.INSTANCE, NbtOps.INSTANCE.empty()).getOrThrow());
         return nbt;
     }
 
@@ -40,10 +41,10 @@ public class SaveState extends PersistentState {
         return stateManager.getOrCreate(TYPE, "mystical");
     }
 
-    private static SaveState readFromNbt(NbtCompound save) {
-        var result = CODEC.decode(NbtOps.INSTANCE, save.get("mystical_save"));
-        if (result.get().left().isPresent()) { // If we successfully loaded the PersistentState
-            return result.get().left().get().getFirst();
+    private static SaveState readFromNbt(NbtCompound save, RegistryWrapper.WrapperLookup wrapperLookup) {
+        var result = CODEC.decode(NbtOps.INSTANCE, save.get("mystical_save")).result();
+        if (result.isPresent()) { // If we successfully loaded the PersistentState
+            return result.get().getFirst();
         }
         Utils.log("Failed to load PersistentState - this is normal when updating versions from below 4.1.0");
         return null;
