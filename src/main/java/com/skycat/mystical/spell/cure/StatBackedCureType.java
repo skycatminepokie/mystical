34dd72e15a6871ea0a5bb7e9ecf99d0040f467b3
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-05-11 21:15:29 -0700

    MapCodec cure

--- src/main/java/com/skycat/mystical/spell/cure/StatBackedCureType.java
+++ src/main/java/com/skycat/mystical/spell/cure/StatBackedCureType.java
@@ -1,6 +1,7 @@
 package com.skycat.mystical.spell.cure;
 
 import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.skycat.mystical.util.StatCodec;
 import lombok.NonNull;
@@ -10,8 +11,8 @@ import java.util.HashMap;
 
 public class StatBackedCureType implements CureType<StatBackedSpellCure> {
     @Override
-    public @NonNull Codec<StatBackedSpellCure> getCodec() {
-        return RecordCodecBuilder.create(spellCureInstance -> spellCureInstance.group(
+    public @NonNull MapCodec<StatBackedSpellCure> getCodec() {
+        return RecordCodecBuilder.mapCodec(spellCureInstance -> spellCureInstance.group(
                 Codec.INT.fieldOf("contributionGoal").forGetter(StatBackedSpellCure::getContributionGoal),
                 StatCodec.INSTANCE.fieldOf("stat").forGetter(StatBackedSpellCure::getStat),
                 Codec.unboundedMap(Uuids.CODEC, Codec.INT).fieldOf("contributions").forGetter(StatBackedSpellCure::getContributionCopy)
