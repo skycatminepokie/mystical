248d8ae4a0f001a7b8a97b09545542f95ee06067
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-06-26 20:13:58 -0700

    Replace redirect with wrapoperation
    Signed-off-by: skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>

--- src/main/java/com/skycat/mystical/mixin/ChickenEntityMixin.java
+++ src/main/java/com/skycat/mystical/mixin/ChickenEntityMixin.java
@@ -1,24 +1,24 @@
 package com.skycat.mystical.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.skycat.mystical.Mystical;
 import com.skycat.mystical.spell.consequence.TurboChickensConsequence;
 import net.minecraft.entity.passive.ChickenEntity;
 import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ChickenEntity.class)
 public abstract class ChickenEntityMixin {
-    @Redirect(method = "tickMovement", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/passive/ChickenEntity;eggLayTime:I", opcode = Opcodes.PUTFIELD, ordinal = 1)) // TODO: Don't redirect
-    private void mystical_shortenEggLayTime(ChickenEntity instance, int value) {
+    @WrapOperation(method = "tickMovement", at = @At(value = "FIELD", target = "Lnet/minecraft/entity/passive/ChickenEntity;eggLayTime:I", opcode = Opcodes.PUTFIELD, ordinal = 1))
+    private void mystical_shortenEggLayTime(ChickenEntity instance, int value, Operation<Void> original) {
         if (!Mystical.isClientWorld() &&
                 Mystical.getSpellHandler().isConsequenceActive(TurboChickensConsequence.class) &&
                 Mystical.getHavenManager().isInHaven(instance)) {
-            instance.eggLayTime = (int) (value / Mystical.CONFIG.turboChickens.speed());
+            original.call(instance, (int) (value / Mystical.CONFIG.turboChickens.speed()));
         } else {
-            instance.eggLayTime = value;
+            original.call(instance, value);
         }
     }
 }
